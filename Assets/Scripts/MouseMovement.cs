a5542a8fbc592f92d1f22bc981e784d7d499fd3f
Kevin Lin <klin0732@gmail.com>
2022-08-03 21:16:49 -0400

    updated
    changes made

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -6,17 +6,24 @@ public class MouseMovement : MonoBehaviour
 {
     public Vector3 targetPosition;
     public float speed; 
+    private RaycastHit2D hit;
 
     // Update is called once per frame
-    void Update () {
- 
+    void Update ()
+    {
         if(Input.GetKeyDown(KeyCode.Mouse1))
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             targetPosition.z = 0;
         }
- 
+
+        if ((targetPosition.x - transform.position.x)  > 0)
+            transform.localScale = new Vector3(-1,1,1);
+        else if ((targetPosition.x - transform.position.x) < 0)
+            transform.localScale = new Vector3(1,1,1);
+        
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
-        Debug.Log(transform.position);
+
+        Debug.Log(targetPosition - transform.position);
     }
 }
